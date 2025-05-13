eba4b6aaae6f7f45f55bcfc0374d44878c964452
kamil <kamilmaslanka212@gmail.com>
2025-05-13 15:53:01 +0200

    Fonts and images setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,34 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik-Regular", "sans-serif"],
+        "rubik-bold": ["Rubik-Bold", "sans-serif"],
+        "rubik-light": ["Rubik-Light", "sans-serif"],
+        "rubik-medium": ["Rubik-Medium", "sans-serif"],
+        "rubik-extrabold": ["Rubik-ExtraBold", "sans-serif"],
+        "rubik-semibold": ["Rubik-SemiBold", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          100: "#0061FF0A",
+          200: "#0061FF1A",
+          300: "#0061FF",
+        },
+        accent: {
+          100: "#FBFBFB",
+        },
+        black: {
+          DEFAULT: "#000000",
+          100: "#8C8E90",
+          200: "#666876",
+          300: "#191d31",
+        },
+        danger: "#FF3B30",
+        success: "#4CD964",
+      },
+    },
   },
   plugins: [],
 };
