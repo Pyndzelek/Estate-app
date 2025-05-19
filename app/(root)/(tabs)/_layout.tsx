ce6687c72954844c40ee12b429f28d8d517f05ff
kamil <kamilmaslanka212@gmail.com>
2025-05-19 19:01:21 +0200

    tabs setup

---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,85 @@
+import { Tabs } from "expo-router";
+import React from "react";
+import { Image, Text, View } from "react-native";
+
+import iscons from "@/constants/icons";
+
+const TabIcon = ({
+  focused,
+  icon,
+  title,
+}: {
+  focused: boolean;
+  icon: any;
+  title: string;
+}) => {
+  return (
+    <View className="flex-1 mt-3 flex flex-col items-center">
+      <Image
+        source={icon}
+        tintColor={focused ? "#0061FF" : "#666876"}
+        resizeMode="contain"
+        className="size-6"
+      />
+      <Text
+        className={`${
+          focused
+            ? "text-primary-300 font-rubik-medium"
+            : "text-black-200 font-rubik"
+        } text-xs w-full  text-center mt-1`}
+      >
+        {title}
+      </Text>
+    </View>
+  );
+};
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "white",
+          position: "absolute",
+          borderTopColor: "#0061FF1A",
+          borderTopWidth: 1,
+          minHeight: 70,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={iscons.home} title="Home" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "explore",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={iscons.search} title="explore" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={iscons.person} title="profile" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
