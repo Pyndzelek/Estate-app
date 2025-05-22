b57d3c57e8c5bdc3ebb234d70c1c92b9934f2085
kamil <kamilmaslanka212@gmail.com>
2025-05-22 20:01:59 +0200

    Main screen UI

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,38 +1,114 @@
+import { settings } from "@/constants/data";
+import icons from "@/constants/icons";
 import { logout } from "@/lib/appwrite";
 import { useGlobalContext } from "@/lib/global-provider";
 import React from "react";
-import { Alert, Text, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+interface SettingsItemProps {
+  icon: any;
+  title: string;
+  onPress?: () => void;
+  showArrow?: boolean;
+  textStyle?: string;
+}
+const SettingsItem = ({
+  icon,
+  title,
+  textStyle,
+  onPress,
+  showArrow = true,
+}: SettingsItemProps) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      className="flex flex-row items-center justify-between py-3"
+    >
+      <View className="flex flex-row items-center gap-3">
+        <Image source={icon} className="size-6" />
+        <Text
+          className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}
+        >
+          {title}
+        </Text>
+      </View>
+
+      {showArrow && <Image source={icons.rightArrow} className="size-5" />}
+    </TouchableOpacity>
+  );
+};
 
 const Profile = () => {
-  const { refetch } = useGlobalContext();
+  const { refetch, user } = useGlobalContext();
   const handleLogOut = async () => {
     const result = await logout();
     if (result) {
+      Alert.alert("Success", "You have been logged out successfully.");
       refetch();
     } else {
-      Alert.alert("Login failed", "Please try again.");
-      console.log("Login failed");
+      Alert.alert("Logout failed", "Please try again.");
     }
   };
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <TouchableOpacity
-        onPress={handleLogOut}
-        className="bg-white shadow-md shadow-zinc-300 rounded-full w-full mt-5 py-4"
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-32 px-7"
       >
-        <View className="flex-row justify-center items-center mx-5">
-          <Text className="text-lg font-rubik-medium text-black-300 ml-2">
-            Log Out
-          </Text>
+        <View className="flex flex-row items-center justify-between mt-5">
+          <Text className="text-xl font-rubik-bold">Profile</Text>
+          <Image source={icons.bell} className="size-5" />
         </View>
-      </TouchableOpacity>
-    </View>
+
+        <View className="flex flex-row justify-center mt-5">
+          <View className="flex flex-col items-center relative rounded-full">
+            <Image
+              source={{ uri: user?.avatar }}
+              className="size-44 relative rounded-full"
+            />
+            <TouchableOpacity className="absolute bottom-11 right-2">
+              <Image source={icons.edit} className="size-9" />
+            </TouchableOpacity>
+
+            <Text className="text-2xl font-rubik-bold mt-2">{user?.name}</Text>
+          </View>
+        </View>
+
+        <View className="flex flex-col mt-10">
+          <SettingsItem icon={icons.calendar} title="My Bookings" />
+          <SettingsItem icon={icons.wallet} title="Payments" />
+        </View>
+
+        <View className="flex flex-col mt-5 border-t pt-5 border-primary-200">
+          {settings.slice(2).map((item, index) => (
+            <SettingsItem key={index} {...item} />
+          ))}
+        </View>
+
+        <View className="flex flex-col mt-5 border-t pt-5 border-primary-200">
+          <SettingsItem
+            icon={icons.logout}
+            title="Logout"
+            textStyle="text-danger"
+            showArrow={false}
+            onPress={() => {
+              Alert.alert("Logout", "Are you sure you want to logout?", [
+                { text: "Cancel", style: "cancel" },
+                { text: "Log out", onPress: handleLogOut },
+              ]);
+            }}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
